e403a58e47e099d614cb8fba36322c346500869b
dc.demetcelik <dc.demetcelik@hotmail.com>
2021-02-15 15:00:54 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] itemKey;
+        TValue[] itemValue;
+        
+        public MyDictionary()
+        {
+            itemKey = new TKey[0];
+            itemValue = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempArrayKey = itemKey;
+            TValue[] tempArrayValue = itemValue;
+
+            itemKey = new TKey[itemKey.Length + 1];
+            itemValue = new TValue[itemValue.Length + 1];
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                itemKey[i] = tempArrayKey[i];
+                itemValue[i] = tempArrayValue[i];
+
+            }
+
+
+            itemKey[itemKey.Length - 1] = key;
+            itemValue[itemValue.Length - 1] = value;
+
+        }
+
+        public int Count
+        {
+            get { return itemKey.Length; }
+        }
+
+
+        public TKey[] KeyList
+        {
+            get { return itemKey; }
+
+        }
+
+        public TValue[] ValueList
+        {
+            get { return itemValue; }
+
+        }
+    }
+}
+
+
+
+              
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> items = new Dictionary<int, string>();
+
+            items.Add(12, "Demet");
+            items.Add(22, "Aleyna");
+            items.Add(34, "Dilan");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+          
+
+            Console.WriteLine("myItems.Count : " + items.Count);
+
+
+          
+
+            MyDictionary<int, string> myItems = new MyDictionary<int, string>();
+            Console.WriteLine("myItems.Count : " + myItems.Count);
+
+            myItems.Add(12, "Demet");
+            myItems.Add(22, "Aleyna");
+            myItems.Add(34, "Dilan");
+
+            foreach (var item in myItems.KeyList)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            foreach (var item in myItems.ValueList)
+            {
+                Console.WriteLine(item);
+
+            }
+
+
+
+            Console.WriteLine("myItems.Count : " + myItems.Count);
+
+            Console.ReadLine();
+
+        }
+    }
+}
+   
